2a8925e54ae4f0129ceb7ae80524ddaa29c8a8cd
hwang <hgh1057@gmail.com>
2023-06-07 00:19:34 +0900

    dd

--- level_1/day_5.js
+++ level_1/day_5.js
@@ -3,14 +3,16 @@
 function solution(n) {
   let len = n.length;
   let q = [];
-  let nArr = n.split(""); // 문자열을 배열로 변환
+  let arr = n.split(""); // 문자열을 배열로 변환
 
   for (let i = 0; i < len - 4; i++) {
     nArr[i] = "*"; // 해당 인덱스의 값을 '*'로 변경
   }
 
-  q = nArr.join(""); // 배열을 문자열로 변환
+  q = arr.join(""); // 배열을 문자열로 변환
   return q;
 }
 
 console.log(solution("01012345678"));
+
+//다른사람 코드
