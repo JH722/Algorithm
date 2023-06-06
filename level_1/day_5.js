4ff384ff4fb4beb03051578b82ace825005bd1f7
hwang <hgh1057@gmail.com>
2023-06-07 00:20:43 +0900

    dd

--- level_1/day_5.js
+++ level_1/day_5.js
@@ -6,7 +6,7 @@ function solution(n) {
   let arr = n.split(""); // 문자열을 배열로 변환
 
   for (let i = 0; i < len - 4; i++) {
-    nArr[i] = "*"; // 해당 인덱스의 값을 '*'로 변경
+    arr[i] = "*"; // 해당 인덱스의 값을 '*'로 변경
   }
 
   q = arr.join(""); // 배열을 문자열로 변환
