613b827ad607335635e9907c8929ed839b714a9e
hwang <hgh1057@gmail.com>
2023-06-16 00:02:45 +0900

    day_9

--- level_1/day_9.js
+++ level_1/day_9.js
@@ -1,16 +1,16 @@
 function solution(numbers) {
-  const allNumbers = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-  const presentNumbers = new Set(numbers);
-
   let sum = 0;
-
-  for (let num of allNumbers) {
-    if (!presentNumbers.has(num)) {
-      sum += num;
+  for (let i = 0; i < 10; i++) {
+    if (!numbers.includes(i)) {
+      sum += i;
     }
   }
-
   return sum;
 }
 
-console.log(solution([1, 3, 4, 5, 6, 8]));
+console.log(solution([1, 2, 3, 4, 5, 6, 7]));
+
+//
+function solution(numbers) {
+  return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
+}
